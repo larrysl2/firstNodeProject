0e17762bca9afc94e23ff87f5731d2d11c1b28b4
Larry <109638483+larrysl2@users.noreply.github.com>
2023-01-02 12:27:36 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const http = require('http')
+const fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+http.createServer((req,res) => {
+   
+    if (req.url === '/') {
+        fs.readFile('index.html', function(err,data){
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    }
+    if (req.url === '/about') {
+        fs.readFile('about.html', function(err,data){
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    }
+    if (req.url === '/contact-me') {
+        fs.readFile('contact-me.html', function(err,data){
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    }
+    else{
+        fs.readFile('404.html', function(err,data){
+            if(err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
+        });
+    }
+}).listen(3000,()=>{
+        console.log('Listen');
+});
+
